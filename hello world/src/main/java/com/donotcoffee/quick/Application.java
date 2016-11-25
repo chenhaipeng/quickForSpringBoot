557c638ae68af801ba5f1df60fad899212bb11b8
chenhaipeng <369161356@qq.com>
2016-11-25 21:32:34 +0800

    Initial commit

---
+++ hello world/src/main/java/com/donotcoffee/quick/Application.java
@@ -0,0 +1,16 @@
+package com.donotcoffee.quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenhaipeng
+ * @version 1.0
+ * @date 2016/11/25 15:59
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class,args);
+	}
+}
---
+++ hello world/src/main/java/com/donotcoffee/quick/Example.java
@@ -0,0 +1,13 @@
+package com.donotcoffee.quick;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Example {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+}
